4e644ec51e67fb52a4e0c1e213a9193133d37d01
Christian Lisangola <jochri3@gmail.com>
2021-11-20 07:50:33 +0300

    Make http call and add generic type to axios

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,60 +1,38 @@
 import React from "react";
+import axios from "axios";
 
-const contacts = [
-  {
-    _id: "607cf129311fcb0015dd6e89",
-    first_name: "Yukihiro",
-    last_name: "Matsumoto Fumbwa",
-    email: "Matsumoto@gmail.com",
-    phone_number: "+243811609403",
-    position: "Teacher and Software Engineer",
-    work_address: "Tokyo",
-  },
-  {
-    _id: "607cf19e311fcb0015dd6e8b",
-    first_name: "Linus",
-    last_name: "Torvalds",
-    email: "linux@gmail.com",
-    phone_number: "+33878578477",
-    position: "Software Engineer",
-    work_address: "Helsinki",
-  },
-  {
-    _id: "6091374a5516790015430f3a",
-    first_name: "Vance",
-    last_name: "Carney",
-    email: "zygave@mailinator.com",
-    phone_number: "+243818636535",
-    position: "Sint voluptatem dist",
-    work_address: "Veniam eaque offici",
-  },
-  {
-    _id: "60914868c3c58500158f9927",
-    first_name: "Zelda",
-    last_name: "Wolf",
-    email: "ziky@mailinator.com",
-    phone_number: "+243819621840",
-    position: "Aliquam deserunt pra",
-    work_address: "Debitis dolorem dolo",
-  },
-  {
-    _id: "6091487dc3c58500158f9928",
-    first_name: "Thomas",
-    last_name: "Kim",
-    email: "geron@mailinator.com",
-    phone_number: "+243810884055",
-    position: "Adipisci deserunt ar",
-    work_address: "Minima enim dolore i",
-  },
-];
+interface IContact {
+  id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone_number: string;
+  position: string;
+  work_address: string;
+}
 
 const App: React.FC = () => {
   // Mise en place du type static
-  const contactsState = React.useState(contacts);
+  const contactsState = React.useState<IContact[]>([]);
 
-  const deleteContact = (_id: string) => {
+  // Mise en place du typage static
+  async function fetchContacts() {
+    const contacts = await axios
+      .get<IContact[]>("http://localhost:3333/contacts")
+      .then((res) => res.data);
+    return contacts;
+  }
+
+  React.useEffect(() => {
+    (async () => {
+      const contacts = await fetchContacts().then((data) => data);
+      contactsState[1](contacts);
+    })();
+  }, []);
+
+  const deleteContact = (id: string) => {
     const newState = [...contactsState[0]]; //Creating new array from existing copy
-    const index = newState.findIndex((contact) => contact._id === _id);
+    const index = newState.findIndex((contact) => contact.id === id);
     newState.splice(index, 1);
     contactsState[1](newState);
   };
@@ -73,14 +51,14 @@ const App: React.FC = () => {
       <tbody>
         {/* Expliquer code ES6 */}
         {contactsState[0].map((contact, index) => (
-          <tr key={contact._id}>
+          <tr key={contact.id}>
             <td>{contact.last_name}</td>
             <td>{contact.first_name}</td>
             <td>{contact.email}</td>
             <td>{contact.position}</td>
             <td>{contact.phone_number}</td>
             <td>
-              <button onClick={() => deleteContact(contact._id)}>
+              <button onClick={() => deleteContact(contact.id)}>
                 Supprimer
               </button>
             </td>
