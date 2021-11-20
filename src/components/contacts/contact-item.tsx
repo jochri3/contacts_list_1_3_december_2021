1a582c0bcad1599a927719ed091c5073727fa424
Christian Lisangola <jochri3@gmail.com>
2021-11-20 08:13:27 +0300

    add ContactItem component

---
+++ src/components/contacts/contact-item.tsx
@@ -0,0 +1,25 @@
+import IContact from "../../interfaces/i-contacts";
+
+interface ContactItemProps {
+  contact: IContact;
+  deleteContact: (id: string) => void;
+}
+
+const ContactItem: React.FC<ContactItemProps> = (props) => {
+  return (
+    <tr key={props.contact.id}>
+      <td>{props.contact.last_name}</td>
+      <td>{props.contact.first_name}</td>
+      <td>{props.contact.email}</td>
+      <td>{props.contact.position}</td>
+      <td>{props.contact.phone_number}</td>
+      <td>
+        <button onClick={() => props.deleteContact(props.contact.id)}>
+          Supprimer
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default ContactItem;
