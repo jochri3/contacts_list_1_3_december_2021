fa2a15e572657c748c2d8be1db64518b60673618
Christian Lisangola <jochri3@gmail.com>
2021-11-21 09:39:56 +0300

    add fetchContacts action creator

---
+++ src/state/contact/contact.action-creator.ts
@@ -0,0 +1,20 @@
+import { Dispatch } from "react";
+import ContactAPI from "../../config/api";
+import IContact from "../../interfaces/i-contacts";
+import { ActionTypes } from "./contact.action-types";
+import { Action } from "./contact.actions";
+
+export const fetchContacts = () => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({ type: ActionTypes.FETCH_CONTACTS_REQUEST });
+    try {
+      const response = await ContactAPI.get<Array<IContact>>("/");
+      dispatch({
+        type: ActionTypes.FETCH_CONTACTS_SUCESS,
+        payload: response.data,
+      });
+    } catch (error) {
+      dispatch({ type: ActionTypes.FETCH_CONTACTS_FAILURE, payload: "erreur" });
+    }
+  };
+};
